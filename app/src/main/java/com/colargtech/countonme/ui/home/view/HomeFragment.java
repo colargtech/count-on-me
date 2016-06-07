7b9adb8b44eaea8f44ff478383e529d4f843d66d
juanchosaravia@gmail.com <juanchosaravia@gmail.com>
2016-06-07 18:04:54 -0300

    Issue #4: adding fab button to add groups and coordinator layout to hide it on scroll

--- app/src/main/java/com/colargtech/countonme/ui/home/view/HomeFragment.java
+++ app/src/main/java/com/colargtech/countonme/ui/home/view/HomeFragment.java
@@ -2,6 +2,8 @@ package com.colargtech.countonme.ui.home.view;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -26,6 +28,7 @@ public class HomeFragment extends BaseFragment implements HomeView {
 
     private RecyclerView homeList;
     private GroupsAdapter adapter;
+    private FloatingActionButton addGroupButton;
 
     static HomeFragment newInstance() {
         return new HomeFragment();
@@ -44,6 +47,15 @@ public class HomeFragment extends BaseFragment implements HomeView {
         homeList.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         adapter = new GroupsAdapter();
         homeList.setAdapter(adapter);
+
+        addGroupButton = (FloatingActionButton) getView().findViewById(R.id.home_fab_add_group);
+        addGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Add group
+                Snackbar.make(v, "Add Group!", Snackbar.LENGTH_SHORT).show();
+            }
+        });
 
         homePresenter = new HomePresenter();
         homePresenter.attachView(this);
